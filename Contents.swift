13a59fcce33eb26fd6d3e5586ef5b331d504a116
Bmar21 <brandonmalor21@gmail.com>
2016-09-15 12:55:46 -0600

    first playground

---
+++ Contents.swift
@@ -0,0 +1,46 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var thisIsANumber = 79709
+print(thisIsANumber)
+print(str + "words")
+print(str + "\(thisIsANumber)")
+print(thisIsANumber + 79709)
+
+let Sword = "Dukes Wrath"
+
+var sit = "the couch"
+
+var welcomeMessage: String
+welcomeMessage = "Hello"
+
+var red, green, blue: Double
+
+let 🌲 = "Pine Tree"
+
+var friendlyWelcome = "Hello!"
+friendlyWelcome = "Bonjour!"
+// friendlywelcome is now "Bonjour!"
+
+print(friendlyWelcome)
+// Prints "Bonjour"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
